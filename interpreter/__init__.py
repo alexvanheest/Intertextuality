cfc7dc47b5318322ba589de494ee0f783e58bfb9
Alex Van Heest <ajv218@lehigh.edu>
2017-06-27 23:18:56 -0400

    Added context-metadata lyric word list creation method; added nltk dependency

--- interpreter/__init__.py
+++ interpreter/__init__.py
@@ -1,6 +1,7 @@
 import matchers
 from wordlists import Wordlists
 import intertext
+from lyric_word_lists import build_lyric_list_wiki_context
 
 
 # Build trie from saved wordlists:
@@ -9,10 +10,15 @@ big_trie = intertext.build_trie_from_wordlists()
 # Compare two songs given just their filepaths:
 #intertext.compare_songs("../lyrics/nas-1994-illmatic/10-it-aint-hard-to-tell.txt", "../lyrics/survival-of-the-fittest-mobb-deep.txt")
 intertext.compare_songs_wiki("Nas", "It Aint Hard To Tell", "Mobb Deep", "Survival of the Fittest")
-
+#
 # Compare a song to wordlists:
 intertext.compare_song_to_trie("../lyrics/survival-of-the-fittest-mobb-deep.txt", big_trie)
 intertext.compare_song_to_trie_wiki("Mobb Deep", "Survival of the Fittest", big_trie)
+
+context_list = build_lyric_list_wiki_context("Nas", "Halftime")
+# print "should be word:\t" + context_list[3][0]
+# print "should be POS:\t" + context_list[3][3]
+# print "should be line no and word no:\t" + str(context_list[3][1]) + ":" + str(context_list[3][2])
 
 # TODO: Look into using a Wikipedia API / package to avoid needing so many wordlists.
 # To make this effective, we'll need to combine words to nearby ones to develop some sense of
@@ -24,11 +30,7 @@ intertext.compare_song_to_trie_wiki("Mobb Deep", "Survival of the Fittest", big_
 # TODO: For database additions, maybe offer a supervised / unsupervised mode?
 # In supervised mode, it'll present each reference and check with user to see if it should be
 # added. In unsupervised mode, it'll just auto-add things.
-# TODO: Support context-based searches. If we ever want this, it's better to do sooner.
-# Instead of importing just a list of words, make this list into a tuple: (word, (LINE_NO, WORD_NO))
-# This will make getting context a breeze, since we can search down to the word position to find words
-# most related to it. We can use tokenization of the line to find significant words (NNPs, verbs) that
-# can help us make a firmer decision on what's what.
+# TODO: Add testing suite to project.
 # TODO: Look into mapping out themes across songs or albums.
 # We could approach this once we have our categorization technique figured out.
 # TODO: Look into other NLP projects to see what we can work into this project.
--- interpreter/lyric_word_lists.py
+++ interpreter/lyric_word_lists.py
@@ -1,4 +1,5 @@
 from PyLyrics import PyLyrics
+from nltk import pos_tag, word_tokenize
 
 # Contains all methods to do with lyric word lists.
 # To clarify, when I use the phrase "lyric word lists," I'm referring to something very specific:
@@ -22,5 +23,18 @@ def build_lyric_list_wiki(artist, song):
 # Method that performs standard replacements and list-ifies words in string-formatted lyrics
 def replace_and_listify(lyric_word_list):
     return lyric_word_list.replace("[", "").replace("]", "").replace(":", "").replace("\n", " ") \
-            .replace("?", "").replace(",", "").replace(".", "").replace("!", "") \
-            .replace("\"", "").replace("'", "").replace("-", "").split(" ")
+                          .replace("?", "").replace(",", "").replace(".", "").replace("!", "") \
+                          .replace("\"", "").replace("'", "").replace("-", "").split(" ")
+
+
+# Method to build a lyric list with wiki implementation and including line number and word number on line
+def build_lyric_list_wiki_context(artist, song):
+    lyrics_as_string = PyLyrics.getLyrics(artist, song)
+    lyrics_as_lines = lyrics_as_string.split("\n")
+    ret_list = []   # format of each word entry: [ word, line_no, word_no, POS ]
+    for jdx in range(0,len(lyrics_as_lines)):
+        # part of speech tag each line using nltk so we can add that to the context metadata for each word
+        tokd_line = pos_tag(word_tokenize(lyrics_as_lines[jdx]))
+        for idx in range(0,len(lyrics_as_lines[jdx].split())):
+            ret_list.append([tokd_line[idx][0], jdx, idx, tokd_line[idx][1]])
+    return ret_list
